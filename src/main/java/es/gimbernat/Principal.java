524e122c68043986c41f38a9552770e573666a59
alex <alex.carreras.g@gmail.com>
2026-04-07 23:15:46 +0200

    Conexión a BD funcionando con MySQL driver

--- src/main/java/es/gimbernat/Principal.java
+++ src/main/java/es/gimbernat/Principal.java
@@ -3,5 +3,8 @@ package es.gimbernat;
 public class Principal {
     public static void main(String[] args){
         System.out.println("Hola mundo");
+        BBDD bbdd = new BBDD();
+        boolean ok = bbdd.init();
+        System.out.println("Conexión establecida: " + ok);
     }
 }
